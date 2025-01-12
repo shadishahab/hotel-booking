44672a4700739d05a9963455e230d002082cd85c
shadishahab <shadi.shahabrazavi@gmail.com>
2025-01-12 16:36:56 +0330

    Fix get_available_room for edge cases

--- core/utils.py
+++ core/utils.py
@@ -17,11 +17,13 @@ def get_available_room(hotel, start_at, end_at):
     Returns the first available room in the given hotel for the specified date range.
     Raises a ValueError if no rooms are available.
     """
-    rooms = Room.objects.filter(hotel=hotel)
+    rooms = Room.objects.filter(hotel=hotel).select_for_update()
     for room in rooms:
         reservations = Reservation.objects.filter(room=room)
         for reservation in reservations:
             if (reservation.start_at<end_at) and (reservation.end_at>start_at):
+                break
+            if (reservation.start_at == start_at) and (reservation.end_at == end_at):
                 break
         else:
             return room
